921e00b6fe689d3835002a4f25fca4c7e4a61979
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-04-25 12:34:42 +0300

    products list and models/Displa offers to the homepage

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -15,7 +15,7 @@ function Header() {
     return route === router.pathname;
   }
   return (
-    <Menu fluid id="menu" inverted>
+    <Menu stackable fluid id="menu" inverted>
       <Container text>
         <Link href="/">
           <Menu.Item header active={isActive('/')}>
