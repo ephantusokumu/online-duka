46842d16c3d642a7e0e3ae55b94ab9b7ff6acb14
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-04-25 16:04:03 +0300

    product details pages

--- components/Index/ProductList.js
+++ components/Index/ProductList.js
@@ -6,7 +6,7 @@ function ProductList({products}) {
       header: product.name,
       image: product.mediaUrl,
       meta: `Ksh${product.price}`,
-      color: 'teal',
+      color: 'yellow',
       fluid: true,
       childKey: product._id,
       href:`/product?_id=${product._id}`
