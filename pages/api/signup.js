78b2765599e1461a5bfad7e53dd1021bf2b8ae6c
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-04-28 14:40:04 +0300

    sign up form validation

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,12 +2,24 @@ import connectDb from "../../utils/connectDb";
 import User from "../../models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import isEmail from 'validator/lib/isLength'
+import isLength from 'validator/lib/isLength'
 
 connectDb();
 
 export default async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    //0 Validate forms
+    if(!isLength(name, {min: 4, max: 12})){
+      return res.status(422).send("The name must be between 4 - 10 letters")
+    }
+    else if(!isLength(password, {min: 8})){
+      return res.status(422).send("The password must be atleast 8 characters")
+    } else if(!isEmail(email)){
+      return res.status(422).send("Please provide a valid email address")
+    }
+
     // 1) Check to see if the user already exists in the db
     const user = await User.findOne({ email });
     if (user) {
