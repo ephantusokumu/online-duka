150b1bf476e3567e9502f3bf9d0585c708d7962e
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-04-29 15:40:11 +0300

    usertoken auth protect route

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -1,35 +1,35 @@
 import { Menu, Container, Image, Icon } from "semantic-ui-react";
 import Link from "next/link";
-import Router, {useRouter} from 'next/router';
-import NProgress from 'nprogress';
+import Router, { useRouter } from "next/router";
+import NProgress from "nprogress";
 
-Router.onRouteChangeStart = () => NProgress.start()
-Router.onRouteChangeComplete = () => NProgress.done()
-Router.onRouteChangeError = () => NProgress.done()
+Router.onRouteChangeStart = () => NProgress.start();
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
-function Header() {
+function Header({ user }) {
   const router = useRouter();
-  const user = false;
 
-  function isActive(route){
+  function isActive(route) {
     return route === router.pathname;
   }
+
   return (
     <Menu stackable fluid id="menu" inverted>
       <Container text>
         <Link href="/">
-          <Menu.Item header active={isActive('/')}>
+          <Menu.Item header active={isActive("/")}>
             <Image
               size="mini"
               src="/static/duka.svg"
               style={{ marginRight: "1em" }}
             />
-            Online Duka
+            Duka Online
           </Menu.Item>
         </Link>
 
         <Link href="/cart">
-          <Menu.Item header active={isActive('/cart')}>
+          <Menu.Item header active={isActive("/cart")}>
             <Icon name="cart" size="large" />
             Cart
           </Menu.Item>
@@ -37,7 +37,7 @@ function Header() {
 
         {user && (
           <Link href="/create">
-            <Menu.Item header active={isActive('/create')}>
+            <Menu.Item header active={isActive("/create")}>
               <Icon name="add square" size="large" />
               Create
             </Menu.Item>
@@ -47,13 +47,13 @@ function Header() {
         {user ? (
           <>
             <Link href="/account">
-              <Menu.Item header active={isActive('/account')}>
+              <Menu.Item header active={isActive("/account")}>
                 <Icon name="user" size="large" />
                 Account
               </Menu.Item>
             </Link>
 
-            <Menu.Item header >
+            <Menu.Item header>
               <Icon name="sign out" size="large" />
               Logout
             </Menu.Item>
@@ -61,14 +61,14 @@ function Header() {
         ) : (
           <>
             <Link href="/login">
-              <Menu.Item header active={isActive('/login')}>
+              <Menu.Item header active={isActive("/login")}>
                 <Icon name="sign in" size="large" />
                 Login
               </Menu.Item>
             </Link>
 
             <Link href="/signup">
-              <Menu.Item header active={isActive('/signup')}>
+              <Menu.Item header active={isActive("/signup")}>
                 <Icon name="signup" size="large" />
                 Signup
               </Menu.Item>
