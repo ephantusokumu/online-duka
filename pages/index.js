510140175e8061379549047dbaf0488659b075d3
Ephantus Okumu <ephantus.okumu@interintel.co.ke>
2020-05-05 11:56:40 +0300

    pagination

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import axios from 'axios';
-import ProductList from '../components/Index/ProductList'
-import baseUrl from '../utils/baseUrl'
+import React from "react";
+import axios from "axios";
+import ProductList from "../components/Index/ProductList";
+import ProductPagination from "../components/Index/ProductPagination";
+import baseUrl from "../utils/baseUrl";
 
-
-
-
-function Home({products}) {
-
-   return <ProductList products={products} />
+function Home({ products, totalPages }) {
+  return (
+    <>
+      <ProductList products={products} />
+      <ProductPagination totalPages={totalPages} />
+    </>
+  );
 }
 
-Home.getInitialProps = async() =>{
-  const url = `${baseUrl}/api/products`
-    const response = await axios.get(url);
-    return {products: response.data};
-}
-
+Home.getInitialProps = async ctx => {
+  const page = ctx.query.page ? ctx.query.page : "1";
+  const size = 9;
+  const url = `${baseUrl}/api/products`;
+  const payload = { params: { page, size } };
+  // fetch data on server
+  const response = await axios.get(url, payload);
+  // return response data as an object
+  return response.data;
+  // note: this object will be merged with existing props
+};
 
 export default Home;
